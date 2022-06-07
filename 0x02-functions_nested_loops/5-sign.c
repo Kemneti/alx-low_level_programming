10987384050d19131b76ce7e4f0f5763e9c44a8e
Kemneti <oliseikuka1@gmail.com>
2022-06-07 14:45:25 -0700

    debug

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@
  * -1 if number is negative
  */
 int print_sign(int n)
-{
+{ 
 	int value;
 
 	if (n > 0)
@@ -20,6 +20,11 @@ int print_sign(int n)
 	{
 		value = 0;
 		_putchar('0');
+	}
+	else 
+	{
+		value = -1;
+		_putchar('-');
 	}
 
 	return (value);
