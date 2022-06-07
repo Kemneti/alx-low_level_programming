bf50a3b2f09086e52ac4ab917b0a247d17b84b50
Kemneti <oliseikuka1@gmail.com>
2022-06-07 12:23:16 -0700

    _putchar

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,7 +6,7 @@
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
- */
+ **/
 int _islower(int c)
 {
 	char i;
