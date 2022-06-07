ea8bd3eac8e360389faf9edfd94a0a4cb423b126
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-07 18:53:38 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_alphabet_x10 - Check main.h
- * Description: function uses _putchar function to print
- * alphabet in lowercase 10 times
- * Return: Nothing.
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
-	char i;
-	int j;
+	char c, i;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 		_putchar('\n');
 	}
 }
