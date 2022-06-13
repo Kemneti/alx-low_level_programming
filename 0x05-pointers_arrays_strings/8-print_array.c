7254ddf67f1b4bc5572490eff1016a54c1de45b8
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-14 00:03:27 +0100

    Elements in an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,13 @@
  */
 void print_array(int *a, int n)
 {
-  
-	int i;
-	
-	for (i = 0; i < n; i++)
+	int i = 0;
+
+	for (; i < n; i++)
 	{
-		printf(" %d \n", a[i]);
+		printf("%d", a[i]);
 		if (i < n - 1)
 			printf(", ");
-	}	
-	printf("\n");
+	}
+	putchar('\n');
 }
