3c3fabb49a15a60bec8c2c6086d39afb119b1369
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-13 17:22:09 +0100

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: String
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+  int i, len, temp;
+  len = strlen(s);
+  for (i = 0; i < len/2; i++)
+  temp = s[i]; 
+  s[i] = s[len - i - 1]; 
+  s[len - i - 1] = temp; 
+  } 
+ }
