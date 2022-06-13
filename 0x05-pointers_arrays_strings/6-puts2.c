9c43faf2cf61a76a84b56e6bf80079905b67606d
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-13 20:12:41 +0100

    Odd String

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts2(char *str)
+for (int i = 0; str[i]; i++) {
+if (i % 2 != 0) {
+_putchar(str[i]);
+    
+_putchar('\n');
+    
+}
+}
