19f8929f893502031575cba9f3ec313dfba4f4c3
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-13 18:14:28 +0100

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,10 +17,9 @@ void print_rev(char *s)
     
     for(i=n-1;i>=0;i--)
     {
-        	 _putchar(str[i]);
+        	 __putchar(s[--len]);
 
-   _putchar('\n');
-
+	_putchar('\n');
 	
        
     }
