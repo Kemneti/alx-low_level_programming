11f72ba4e4b83a2f5fb4fc0b03dec02a0768ffc3
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-13 20:46:35 +0100

    Odd Char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,12 @@
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i]; i++) {
-if (i % 2 == 0) {
+for (i = 0; str[i]; i++)
+if (i % 2 == 0)
 _putchar(str[i]);
     
 _putchar('\n');
     
-}
-}
+
+
 }
