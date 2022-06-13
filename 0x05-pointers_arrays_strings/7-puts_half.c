d36a53c0817c6c6b11d1f7afb5d8877acc00328b
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-13 22:16:26 +0100

    Right half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+char rightHalf[100];
+int length, mid, i, k,n;
+
+length = strlen(str);
+
+mid = length/2;
+for(i = mid, k = 0; i <= length; i++, k++) 
+rightHalf[k]= str[i];
+if (i % 2 == 0)
+n = (length - 1) / 2
+_putchar(str[i]);
+
+_putchar('\n');
+
+
+}
