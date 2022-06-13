124a75da8291438b13e16d067c6ecae7a7fed5a2
Kemneti <oliseikuka1@gmail.com>
2022-06-13 06:50:53 -0700

    My func  swap commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Function that swaps the values of two integers
+ * @a: An integer to be swaped
+ * @b: An integer to be swaped
+ * Return: Nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int tempnum;
+
+	tempnum = *a;
+	*a = *b;
+	*b = *tempnum;
+}
