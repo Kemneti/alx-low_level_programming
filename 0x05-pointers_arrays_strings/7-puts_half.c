168dd3f5d46d0f682287b0362f46add312ca6973
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-13 22:42:19 +0100

    Right half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ mid = length/2;
 for(i = mid, k = 0; i <= length; i++, k++) 
 rightHalf[k]= str[i];
 if (i % 2 == 0)
-for ((i = (length - 1) / 2; str[i] != 0; i++)
+for ((i = (length - 1) / 2); str[i] != 0; i++)
 _putchar(str[i]);
 
 _putchar('\n');
