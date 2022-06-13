4f486096f779f4a2821fec9f1b92da5addca5539
Kemneti <oliseikuka1@gmail.com>
2022-06-13 07:25:32 -0700

    My count swap commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _strlen - function that returns the length of a string
+ * @s: The string 
+ * Return: Nothing
+ *
+int _strlen(char *s)
+	char s[] = "Programming is fun"
