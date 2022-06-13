63f53a57cc56601c4f0b8eb1c82361532346b72c
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-14 00:01:31 +0100

    Elements of ana array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,6 +15,9 @@ void print_array(int *a, int n)
 	
 	for (i = 0; i < n; i++)
 	{
-		printf(" Element at Array[%d] = %d \n", i, a[i]);
+		printf(" %d \n", a[i]);
+		if (i < n - 1)
+			printf(", ");
 	}	
+	printf("\n");
 }
