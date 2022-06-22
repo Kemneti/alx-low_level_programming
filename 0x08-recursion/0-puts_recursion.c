9378ce88a183073d18795eecb485ea6da8b5bb82
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-22 12:13:03 +0100

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	unsigned int index = 0;
+
+	if (s[index] == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(s[index]);
+	_puts_recursion(s + 1);
 }
