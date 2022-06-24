c3f51763c17de7de70b6b07b05abff8c0819421a
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-06-24 18:46:58 +0100

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,17 @@
-#!/bin/bash
-gcc -c *.c
-ar cr liball.a *.o
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of commandline arguments
+ * @argv: list of commandline arguments
+ *
+ * Return: 0 Always(success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	(void)argc;
+
+	return (0);
+}
